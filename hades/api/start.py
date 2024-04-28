bb2cc4c1774e17f4cfe79cab8653a14ff833011a
alluding <149972831+alluding@users.noreply.github.com>
2024-04-28 11:47:29 -0400

    Update start.py

--- hades/api/start.py
+++ hades/api/start.py
@@ -1,77 +1,41 @@
 from __future__ import annotations
-from typing import (
-    Dict,
-    Optional,
-    List,
-    TypeVar,
-    TypedDict
+from typing import Dict, List
+
+from discord import (
+    User,
+    Member,
+    DMChannel,
+    Message
 )
+from discord.ext.commands import group, command, Cog
 
-from flask import (
-    Flask,
-    request,
-    Response,
-    render_template
-)
+from ..managers.context import HadesContext, Flags
+from ..managers.embed import Embed
+from ..hades import Hades
 
-from pydantic import BaseModel
-from enum import Enum
-
-import json
-import os
-
-app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+import asyncio
 
 
-class EmbedPayload(BaseModel):
-    redirect: Optional[str] = None
-    title: str
-    url: str = "https://discord.com/"
-    color: Optional[str] = None
-    image: Optional[str] = None
-    thumbnail: Optional[str] = None
-    description: str
+class Profile(Cog):
+    def __init__(self: Hades, bot: Hades) -> None:
+        self.bot: Hades = bot
 
-
-class Database(TypedDict):
-    embeds: Dict[str, List[str]]
-
-
-class EmbedStatus(Enum):
-    EMBED_NOT_FOUND = "Embed not found"
-
-
-db: Database[EmbedPayload] = Database(json.load(open("./hades/api/database.json", "r")))
-
-
-@app.route("/create", methods=["POST"])
-def create_embed() -> Dict[str, str]:
-    _id = os.urandom(5).hex()
-    payload = EmbedPayload(**request.json)
-    db["embeds"][_id] = payload.__dict__
-    
-    with open("./hades/api/database.json", "w") as f:
-        json.dump(db, f)
-        
-    return {"id": _id}
-
-
-@app.route("/<_id>")
-def fetch_embed(_id: str) -> Response:
-    embed = db["embeds"].get(_id)
-    if embed:
-        embed = EmbedPayload(**embed)
-        embed.thumbnail = bool(embed.thumbnail)
-        return render_template("template.html", **embed.dict())
-    else:
-        return Response(response=json.dumps({"message": "Embed not found"}), status=404)
-
-
-if __name__ == "__main__":
-    app.run(
-        port=5000,
-        host="0.0.0.0",
-        debug=True
+    @command(
+        name="bio",
+        description="Change your profile's bio!",
+        example="My new bio.",
+        usage="(bio)"
     )
+    async def bio(self: Profile, ctx: HadesContext, *, bio: str) -> Message:
+        await ctx.message.delete()
+        await self.bot.user.edit(bio=bio)
+
+        return await ctx.do(
+            _type=Flags.NEUTRAL,
+            emoji="📖",
+            content=f"Bio successfully changed to: {bio}"
+        )
+
+
+async def setup(bot: Hades) -> None:
+    await bot.add_cog(Profile(bot))
