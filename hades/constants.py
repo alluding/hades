689f096ce424805fc53b579650218b22c524ade2
dancers <149972831+alluding@users.noreply.github.com>
2024-04-29 05:02:23 -0400

    constants

---
+++ hades/constants.py
@@ -0,0 +1,12 @@
+from typing import Dict, Any
+
+from discord import HypeSquadHouse
+import re
+
+HYPESQUAD: Dict[str, Any] = {
+    "balance": HypeSquadHouse.balance,
+    "bravery": HypeSquadHouse.bravery,
+    "brilliance": HypeSquadHouse.brilliance
+}
+NITRO_REGEX = re.compile(r"(discord.com/gifts/|discordapp.com/gifts/|discord.gift/)([a-zA-Z0-9]+)")
+PRIVNOTE_REGEX = re.compile(r"https://privnote\.com/[a-zA-Z0-9]+#[a-zA-Z0-9]+")
