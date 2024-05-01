dd7d292705d2eee68a57fd3d5823e1bbd40dd943
dancers <149972831+alluding@users.noreply.github.com>
2024-05-01 10:43:37 -0400

    sigh

--- hades/util.py
+++ hades/util.py
@@ -90,16 +90,16 @@ def read_note(url: str) -> str:
         password=password
     ) if password else "No password found"
 
-def check_package_exists(package_name: str) -> str:
-    installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
-    return "installed" if package_name.lower() in installed_packages.lower() else "not installed"
+# def check_package_exists(package_name: str) -> str:
+#     installed_packages = subprocess.check_output(["pip", "list"]).decode("utf-8")
+#     return "installed" if package_name.lower() in installed_packages.lower() else "not installed"
 
-def pip_install(*packages: str, check_exists: bool = False) -> None:
-    package_statuses = {package: check_package_exists(package) for package in packages}
+# def pip_install(*packages: str, check_exists: bool = False) -> None:
+#     package_statuses = {package: check_package_exists(package) for package in packages}
 
-    for package, status in package_statuses.items():
-        if check_exists and status == "installed":
-            print(f"{package} is already installed.")
-        else:
-            subprocess.run(["pip", "install", package])
-            print(f"{package} has been successfully installed.")
+#     for package, status in package_statuses.items():
+#         if check_exists and status == "installed":
+#             print(f"{package} is already installed.")
+#         else:
+#             subprocess.run(["pip", "install", package])
+#             print(f"{package} has been successfully installed.")
