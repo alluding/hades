e5c9f4c61c7b51e6e5e7873cecce4fa2c275a9ad
dancers <149972831+alluding@users.noreply.github.com>
2024-05-01 14:18:02 -0400

    Update constants.py

--- hades/constants.py
+++ hades/constants.py
@@ -8,9 +8,15 @@ HYPESQUAD: Dict[str, Any] = {
     "bravery": HypeSquadHouse.bravery,
     "brilliance": HypeSquadHouse.brilliance
 }
+
 PLATFORM: Dict[str, str] = {
     "ps": "psnUsername",
     "xbl": "xboxUsername"
 }
+
+HEADERS: Dict[str, str] = {
+    "X-Requested-With": "XMLHttpRequest",
+}
+
 NITRO_REGEX = re.compile(r"(discord.com/gifts/|discordapp.com/gifts/|discord.gift/)([a-zA-Z0-9]+)")
 PRIVNOTE_REGEX = re.compile(r"https://privnote\.com/[a-zA-Z0-9]+#[a-zA-Z0-9]+")
