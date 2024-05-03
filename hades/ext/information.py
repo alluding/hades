8f00977020ab29ae9e6e50191b2cb06ade0bbaae
dancers <149972831+alluding@users.noreply.github.com>
2024-05-03 00:09:49 -0400

    `xresolver` now forces Cloudflare.

--- hades/ext/information.py
+++ hades/ext/information.py
@@ -11,7 +11,6 @@ from discord.ext.commands import group, command, Cog
 
 from ..managers.context import HadesContext, Flags
 from ..managers.embed import Embed
-from ..util import resolve_user
 from ..hades import Hades
 
 import asyncio
@@ -19,7 +18,6 @@ import asyncio
 class Information(Cog):
     def __init__(self, bot: Hades) -> None:
         self.bot: Hades = bot
-        self.resolve_time: Dict[str, float] = {}
 
     @command(
         name="test"
@@ -30,56 +28,9 @@ class Information(Cog):
         """
         await ctx.do(
             _type=Flags.DENY,
-            content="test"
+            content="test",
+            embed=self.bot.embed
         )
-
-    @command(
-        name="resolve",
-        description="Username to IP address resolver!",
-        usage="(username) (platform)",
-        example="anime ps/xbl"
-    )
-    async def resolve(
-        self: Information,
-        ctx: HadesContext,
-        username: str,
-        platform: Literal["ps", "xbl"]
-    ) -> Message:
-        await ctx.message.delete()
-
-        if ctx.author.id in self.resolve_time and self.resolve_time[ctx.author.id] > asyncio.get_event_loop().time():
-            return await ctx.do(
-                _type=Flags.WARN,
-                content=f"Rate limited for `{round(self.resolve_time[ctx.author.id] - asyncio.get_event_loop().time())}` seconds.",
-                embed=self.bot.embed,
-                delete_after=12
-            )
-
-        data = resolve_user(username, platform)
-
-        if isinstance(data, str) and "Rate limited" in data:
-            self.resolve_time[ctx.author.id] = asyncio.get_event_loop().time() + 1800
-            
-            return await ctx.do(
-                _type=Flags.WARN,
-                content=data,
-                embed=self.bot.embed,
-                delete_after=12
-            )
-            
-        if "user wasn't found" in data:
-            return await ctx.do(
-                _type=Flags.ERROR,
-                content=data,
-                embed=self.bot.embed,
-                delete_after=12
-            )
-            
-        return await ctx.send(
-            content="\n".join([f"**{key}** » `{value}`" for key, value in data.items()]),
-            delete_after=12
-        )
-
 
 async def setup(bot: Hades) -> None:
     await bot.add_cog(Information(bot))
