ca1d60f02aa8a0d614e32f9a5dc99854be225c12
z <149972831+alluding@users.noreply.github.com>
2024-06-24 08:30:07 -0400

    Update clear.py

--- clear.py
+++ clear.py
@@ -1,32 +1,30 @@
+from typing import Union, List
 import os
-from typing import Union
-
 
 def clear_pycache() -> str:
     try:
-        pycache_paths = [
+        pycache_paths: List[str] = [
             os.path.join(root, d)
             for root, dirs, _ in os.walk(os.getcwd())
             for d in dirs if d == "__pycache__"
         ]
 
-        for pycache_path in pycache_paths:
-            print(f"Removing {pycache_path}")
-            items = [
-                os.path.join(pycache_path, item)
-                for item in os.listdir(pycache_path)
+        for pycache in pycache_paths:
+            print(f"Removing {pycache}")
+            
+            items: List[str] = [
+                os.path.join(pycache, item)
+                for item in os.listdir(pycache)
             ]
 
             for item in items:
                 if os.path.isfile(item) or os.path.islink(item):
                     os.unlink(item)
 
-            os.rmdir(pycache_path)
+            os.rmdir(pycache)
 
-        return "Cleanup completed."
+        print("Cleanup completed.")
     except Exception as e:
-        return f"Error: {e}"
+        print(f"Error: {e}")
 
-
-result: Union[str, None] = clear_pycache()
-print(result)
+clear_pycache()
