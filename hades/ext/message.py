5e0239881bfb934a6b02e47912a4543478e7343d
z <149972831+alluding@users.noreply.github.com>
2024-06-25 15:17:14 -0400

    Update message.py

--- hades/ext/message.py
+++ hades/ext/message.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import Dict, List, Literal, Optional
+from pydantic import BaseModel, HttpUrl
 
 from discord import (
     User,
@@ -11,17 +12,45 @@ from discord import (
     errors
 )
 from discord.ext.commands import group, command, Cog
+import discord
 
 from ..managers.context import HadesContext, Flags
 from ..managers.embed import Embed
 from ..hades import Hades
 
 from xxhash import xxh32_hexdigest
+import datetime
 import asyncio
 
 class Messages(Cog):
     def __init__(self, bot: Hades) -> None:
         self.bot: Hades = bot
+
+    @Cog.listener("on_message_delete")
+    async def deletes(self: Messages, message: Message) -> None:
+        if not message.author.bot and message.author != self.bot.user:
+            try:
+                await self.bot.cache.sadd(
+                    f"snipe:{xxh32_hexdigest(str(message.channel.id))}",
+                    self.bot.dump(message),
+                )
+            except Exception as e:
+                self.bot.logger.error(e)
+
+    @Cog.listener("on_message_edit")
+    async def edits(
+        self: Messages,
+        before: Message,
+        after: Message
+    ) -> None:
+        if not after.author.bot and after.author != self.bot.user:
+            try:
+                await self.bot.cache.sadd(
+                    f"editsnipe:{xxh32_hexdigest(str(after.channel.id))}",
+                    [self.bot.dump(before), self.bot.dump(after)],
+                )
+            except Exception as e:
+                self.bot.logger.error(e)
 
     @Cog.listener("on_message")
     async def check_reply(self: Messages, origin: Message) -> None:
@@ -31,10 +60,10 @@ class Messages(Cog):
         reply = await self.bot.cache.get(f"auto_reply:{xxh32_hexdigest(str(self.bot.user.id))}")
 
         if reply and (ref := origin.reference) and (resolved := ref.resolved) and isinstance(resolved, Message):
-            if resolved.author == self.bot.user:
+            if resolved.author == self.bot.user and origin.author != self.bot.user:
                 await origin.reply(reply[0])
         
-        if reply and self.bot.user.mentioned_in(origin):
+        if reply and self.bot.user.mentioned_in(origin) and origin.reference is None:
             await origin.reply(reply[0])
 
     @Cog.listener("on_message")
@@ -63,7 +92,89 @@ class Messages(Cog):
                     print(
                         f"[AUTO-REACT] Forbidden error when reacting to {origin.author.name}'s message: {str(e)}"
                     )
-                    
+
+    @command(
+        name="editsnipe",
+        aliases=["es"],
+        description="Snipe any deleted message/s.",
+        usage="(index)",
+        example="1"
+    )
+    async def editsnipe(
+        self: Messages,
+        ctx: HadesContext,
+        index: int = 1
+    ) -> Message:
+        await ctx.message.delete()
+
+        snipes = await self.bot.cache.get(f"editsnipe:{xxh32_hexdigest(str(ctx.channel.id))}")
+        
+        if not snipes or index <= 0 or index > len(snipes):
+            return await ctx.do(
+                _type=Flags.WARN,
+                emoji="❌",
+                content="There are no edited messages to snipe."
+            )
+        
+        before, after = snipes[0][0], snipes[0][1]
+        
+        return await ctx.send(
+            content=(
+                f"Sniped edited content from **{before['author']['name']}** - {discord.utils.format_dt(after['timestamp'])}:\n\n"
+                f"```\nBefore -> {before['content']}\nAfter -> {after['content']}```\n\n"
+                f"{index}/{len(snipes)} messages sniped.\n"
+            ).strip(),
+            delete_after=5
+        )
+
+    @command(
+        name="snipe",
+        aliases=["s"],
+        description="Snipe any deleted message/s.",
+        usage="(index)",
+        example="1"
+    )
+    async def snipe(
+        self: Messages,
+        ctx: HadesContext,
+        index: int = 1
+    ) -> Message:
+        await ctx.message.delete()
+
+        snipes = await self.bot.cache.get(f"snipe:{xxh32_hexdigest(str(ctx.channel.id))}")
+        
+        if not snipes or index <= 0 or index > len(snipes):
+            return await ctx.do(
+                _type=Flags.WARN,
+                emoji="❌",
+                content="There are no deleted messages to snipe."
+            )
+        
+        message = snipes[index - 1]
+        
+        return await ctx.send(
+            content=(
+                f"Sniped content from **{message['author']['name']}** - {discord.utils.format_dt(message['timestamp'])}:\n\n"
+                f"```\n{message['content']}```\n\n"
+                f"{index}/{len(snipes)} messages sniped.\n"
+            ).strip(),
+            delete_after=5
+        )   
+    
+    @command(
+        name="clearsnipes",
+        aliases=["cs"],
+        description="Clear the snipes of edited & deleted messages."
+    )
+    async def clearsnipes(self: Messages, ctx: HadesContext) -> Message:
+        for query in [
+            f"snipe:{xxh32_hexdigest(str(ctx.channel.id))}",
+            f"editsnipe:{xxh32_hexdigest(str(ctx.channel.id))}"
+        ]:
+            await self.bot.cache.remove(query)
+
+        return await ctx.message.add_reaction("👍")
+
     @command(
         name="autoreply",
         description="Toggle auto-reply for someone.",
@@ -80,6 +191,13 @@ class Messages(Cog):
 
         check: str = "off" if await self.bot.cache.get(f"auto_reply:{xxh32_hexdigest(str(ctx.author.id))}") else "on"
 
+        if not message and check != "off":
+            return await ctx.do(
+                _type=Flags.WARN,
+                emoji="❌",
+                content="Hey! You need a `message` as well!"
+            )
+
         await self.bot.cache.sadd(
             f"auto_reply:{xxh32_hexdigest(str(ctx.author.id))}",
             message
@@ -91,7 +209,7 @@ class Messages(Cog):
         return await ctx.do(
             _type=Flags.APPROVE,
             emoji="✅",
-            content=f"Auto-reply has been turned **{check}** with `{message}` as the message." if check else f"Auto-reply has been turned **{check}**.",
+            content=f"Auto-reply has been turned **{check}** with `{message}` as the message." if check == "off" else f"Auto-reply has been turned **{check}**.",
             embed=self.bot.embed
         )
 
