573010ca0e30ab0a6e6cbcff15c29970acd72eb5
z <149972831+alluding@users.noreply.github.com>
2024-06-25 09:38:12 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from hades.managers.updater import Updater
 from hades.hades import Hades
 
-current: int | float = 1.4
-
+current: int | float = 1.5
 updater: Updater = Updater(current_version=current)
 updater.run()
 
