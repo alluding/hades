292beeb8481a985fa5a3402e0324504312ff0988
z <149972831+alluding@users.noreply.github.com>
2024-06-27 10:31:48 -0400

    Update hades.py

--- hades/hades.py
+++ hades/hades.py
@@ -9,20 +9,18 @@ from typing import (
     TYPE_CHECKING,
     Tuple,
     Optional,
-    ClassVar
+    ClassVar,
 )
 from typing_extensions import override
 
-from discord.ext.commands import Bot
+from discord.ext import commands
 from discord import (
     TextChannel,
     Message,
     Guild,
     User,
-    DMChannel
+    DMChannel,
 )
-from discord.ext import commands
-
 from curl_cffi.requests import Session
 from datetime import datetime
 from pathlib import Path
@@ -42,7 +40,7 @@ if TYPE_CHECKING:
 
 __all__: Tuple[str, ...] = ("Hades",)
 
-class Hades(Bot):
+class Hades(commands.Bot):
     """
     An advanced Discord self-bot made in Python, relying on discord.py-self.
     """
@@ -50,15 +48,14 @@ class Hades(Bot):
     config: ClassVar[Config] = json.load(open("config.json", "r"))
     ready: ClassVar[bool] = False
 
-    def __init__(self, *args: Any, **kwargs: Any) -> Bot:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(
             *args,
-            *kwargs,
+            **kwargs,
             command_prefix=self.get_prefix,
             description="Hades Discord Self-Bot",
             strip_after_prefix=True,
             self_bot=True,
-            help_command=None,
             proxy=self.config["settings"]["proxy"],
         )
 
@@ -73,40 +70,34 @@ class Hades(Bot):
         self.run()
 
     @overload
-    def dump(self: Hades, message: Message) -> Dict[str, Union[Dict[str, Union[int, str, bool]], List[str], float, int]]:
+    def dump(self, message: Message) -> Dict[str, Union[Dict[str, Union[int, str, bool]], List[str], float, int]]:
         ...
 
-    def dump(self: Hades, message: Message) -> Dict[str, Union[str, float, int, List[Dict[str, Union[str, int]]]]]:
-        guild: Guild = message.guild
-        channel: TextChannel = message.channel
+    def dump(self, message: Message) -> Dict[str, Union[str, float, int, List[Dict[str, Union[str, int]]]]]:
+        guild: Optional[Guild] = message.guild
+        channel: Union[TextChannel, DMChannel] = message.channel
         author: User = message.author
 
-        attachments: List[str] = [
-            attachment.url for attachment in (message.attachments + [
-                embed.thumbnail or embed.image
-                for embed in message.embeds if embed.type == "image"
-            ])
+        attachments: List[str] = [attachment.url for attachment in message.attachments] + [
+            embed.thumbnail.url if embed.thumbnail else embed.image.url for embed in message.embeds if embed.type == "image"
         ]
         stickers: List[str] = [sticker.url for sticker in message.stickers]
         embeds: List[Dict[str, Union[str, int]]] = [
-            embed.to_dict()
-            for embed in message.embeds[:8]
-            if embed.type not in ("image", "video")
+            embed.to_dict() for embed in message.embeds[:8] if embed.type not in ("image", "video")
         ]
 
-        def get_channel(channel: Union[TextChannel, DMChannel]):
-            if isinstance(channel, TextChannel):
-                return {
-                    "id": channel.id,
+        channel_info = {
+            "id": channel.id,
+            **(
+                {
                     "name": channel.name,
                     "position": channel.position,
-                    "category_id": channel.category_id
+                    "category_id": channel.category_id,
                 }
-            else:
-                return {
-                    "id": channel.id,
-                    "type": "DM"
-                }
+                if isinstance(channel, TextChannel)
+                else {"type": "DM"}
+            ),
+        }
 
         return {
             "guild": {
@@ -114,16 +105,10 @@ class Hades(Bot):
                 "name": guild.name,
                 "chunked": guild.chunked,
                 "member_count": guild.member_count,
-            } if guild else None,
-            "channel": {
-                "id": channel.id,
-                "name": channel.name,
-                "position": channel.position,
-                "category_id": channel.category_id
-            } if isinstance(channel, TextChannel) else {
-                "id": channel.id,
-                "type": "DM"
-            },
+            }
+            if guild
+            else None,
+            "channel": channel_info,
             "author": {
                 "name": author.name,
                 "id": author.id,
@@ -137,74 +122,55 @@ class Hades(Bot):
             "embeds": embeds,
             "content": message.content,
             "timestamp": datetime.utcfromtimestamp(message.created_at.timestamp()),
-            "id": message.id
+            "id": message.id,
         }
 
     @override
-    async def get_context(
-        self: Hades,
-        origin: Message,
-        *,
-        cls: Optional[type[HadesContext]] = None
-    ) -> HadesContext:
-        return await super().get_context(
-            origin,
-            cls=cls or HadesContext
-        )
+    async def get_context(self, origin: Message, *, cls: Optional[type[HadesContext]] = None) -> HadesContext:
+        return await super().get_context(origin, cls=cls or HadesContext)
 
     @property
-    def extensions(self: Hades) -> List[str]:
+    def extensions(self) -> List[str]:
         return [
             str(ext.with_suffix("")).replace("\\", ".").replace("/", ".")
             for ext in Path("./hades/ext").glob("**/[!__]*.py")
         ]
 
-    def config_logger(self: Hades) -> logging.Logger:
-        logger: logging.Logger = logging.getLogger("discord")
+    def config_logger(self) -> logging.Logger:
+        logger = logging.getLogger("discord")
         logger.setLevel(logging.INFO)
         self.logger: logging.Logger = logger
         return self.logger
 
-    def fetch_uptime(self: Hades) -> Tuple[int, int, int, int]:
-        delta_seconds = round(
-            (datetime.utcnow() - self.start_time).total_seconds())
-        days, remaining = divmod(delta_seconds, 86400)
-        hours, remaining = divmod(remaining, 3600)
-        minutes, seconds = divmod(remaining, 60)
+    def fetch_uptime(self) -> Tuple[int, int, int, int]:
+        delta_seconds = round((datetime.utcnow() - self.start_time).total_seconds())
+        days, hours, minutes, seconds = (
+            divmod(delta_seconds, 86400)[0],
+            *divmod(divmod(delta_seconds, 86400)[1], 3600),
+            *divmod(divmod(divmod(delta_seconds, 86400)[1], 3600)[1], 60),
+        )
         return days, hours, minutes, seconds
 
-    def run(self: Hades) -> Hades:
-        super().run(
-            token=self.config["token"],
-            reconnect=True
-        )
+    def run(self) -> None:
+        super().run(token=self.config["token"], reconnect=True)
 
-    async def on_ready(self: Hades) -> None:
+    async def on_ready(self) -> None:
         self.logger.info(f"Hades | Logged in as {self.user}")
 
         await self.load_extensions()
 
         if not self.ready:
-            self.ready: bool = True
+            self.ready = True
 
-        self.session: Session = Session(
-            impersonate="chrome119",
-            headers={}
-        )
+        self.session = Session(impersonate="chrome119", headers={})
 
-    async def load_ext(
-        self: Hades,
-        name: str,
-        *,
-        package: Optional[str] = None,
-        cache: bool = False
-    ) -> None:
+    async def load_ext(self, name: str, *, package: Optional[str] = None, cache: bool = False) -> None:
         if cache:
             ...
 
         return await super().load_extension(name, package=package)
 
-    async def load_extensions(self: Hades) -> None:
+    async def load_extensions(self) -> None:
         for ext in self.extensions:
             try:
                 await self.load_ext(ext)
@@ -212,46 +178,44 @@ class Hades(Bot):
             except Exception as e:
                 self.logger.error(f"Failed to load {ext}. | {e}")
 
-    async def get_prefix(self: Hades, message: Message) -> Any:
-        guild: Guild = message.guild
-        user: User = message.author
-
+    async def get_prefix(self, message: Message) -> Any:
         prefixes = list(self.config["settings"]["prefixes"])
-
         return commands.when_mentioned_or(*prefixes)(self, message)
 
-    async def on_command_error(self: Hades, ctx: HadesContext, error: Exception) -> Optional[Message]:
-        if type(error) in (
-            commands.CommandNotFound,
-            commands.NotOwner,
-            commands.CheckFailure,
-            commands.DisabledCommand,
-            commands.UserInputError
+    async def on_command_error(self, ctx: HadesContext, error: Exception) -> Optional[Message]:
+        if isinstance(
+            error, (
+                commands.CommandNotFound, 
+                commands.NotOwner, 
+                commands.CheckFailure, 
+                commands.DisabledCommand, 
+                commands.UserInputError
+            )
         ):
             return
-
-        elif isinstance(error, commands.CommandOnCooldown):
+    
+        if isinstance(error, commands.CommandOnCooldown):
             return await ctx.do(
-                _type=Flags.WARN,
-                content=f"This command is on cooldown. Try again in {error.retry_after:.2f} seconds.",
-                emoji="⏳",
-                delete_after=5,
+                _type=Flags.WARN, 
+                content=f"This command is on cooldown. Try again in {error.retry_after:.2f} seconds.", 
+                emoji="⏳", 
+                delete_after=5, 
                 embed=self.embed
             )
 
-        elif isinstance(error, commands.MemberNotFound):
+        if isinstance(error, commands.MemberNotFound):
             return await ctx.do(
-                _type=Flags.WARN,
-                content="I was unable to find that member, or the ID is invalid.",
+                _type=Flags.WARN, 
+                content="I was unable to find that member, or the ID is invalid.", 
+                embed=self.embed
+            )
+        
+        if isinstance(error, commands.UserNotFound):
+            return await ctx.do(
+                _type=Flags.WARN, 
+                content="I was unable to find that user, or the ID is invalid.", 
                 embed=self.embed
             )
 
-        elif isinstance(error, commands.UserNotFound):
-            return await ctx.do(
-                _type=Flags.WARN,
-                content="I was unable to find that user, or the ID is invalid.",
-                embed=self.embed
-            )
-
-        elif isinstance(error, commands.MissingRequiredArgument):
+        if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.send_help(embed=self.embed)
