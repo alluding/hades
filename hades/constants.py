ba2d8e52a8639524baff486b3dbfe4a8b0fe307c
z <149972831+alluding@users.noreply.github.com>
2024-06-27 00:07:50 -0400

    Bigger pack Bible.

--- hades/constants.py
+++ hades/constants.py
@@ -16,6 +16,7 @@ HEADERS: Dict[str, str] = {
 
 NITRO_REGEX: re.Pattern = re.compile(r"(discord.com/gifts/|discordapp.com/gifts/|discord.gift/)([a-zA-Z0-9]+)")
 PRIVNOTE_REGEX: re.Pattern = re.compile(r"https://privnote\.com/[a-zA-Z0-9]+#[a-zA-Z0-9]+")
-PACKS: List[str] = requests.get(
-    "https://raw.githubusercontent.com/nullsx/PACK-BIBLE-S2/main/Another_BKC_Pack_Bible.txt"
-).text.strip().split("\n\n")
+PACKS: List[str] = list(set(
+    requests.get("https://raw.githubusercontent.com/nullsx/PACK-BIBLE-S2/main/Another_BKC_Pack_Bible.txt").text.strip().split("\n\n") +
+    requests.get("https://raw.githubusercontent.com/nullsx/PACK-BIBLE-S2/main/Huge_Pack_Bible.txt").text.strip().split("\n\n")
+))
